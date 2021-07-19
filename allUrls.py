04e5b260072bd4cd94b8f8dcf2d0b66f5dea7a8e
anthonyreis <matheus.anthony1@gmail.com>
2021-07-19 20:11:38 -0300

    adicionando novas urls na lista para percorrer

--- allUrls.py
+++ allUrls.py
@@ -14,22 +14,27 @@ def allUrls(allLinks, file):
                 for link in soup.find_all('a'):
                     newLink = link.get('href')
                     if type(newLink) == str:
-                        if re.match('^http', newLink):
+                        if re.match('^http', newLink) and newLink not in allLinks:
+                            allLinks.append(newLink)
                             file.write(newLink)
                             file.write('\n')
                         elif re.match('^/', newLink):
                             auxLink = url + newLink
-                            file.write(auxLink)
-                            file.write('\n')
+                            if auxLink not in allLinks:
+                                allLinks.append(auxLink)
+                                file.write(auxLink)
+                                file.write('\n')
+                            else:
+                                print(f'URL repetido: {auxLink}')
+                        elif re.match('^http', newLink):
+                            print(f'URL repetido: {newLink}')
                         
         file.flush()
         file.close()
-        removeDuplicate('links.txt')
     except Exception as ex:
         template = "An exception of type {0} occurred. Arguments:\n{1!r}"
         message = template.format(type(ex).__name__, ex.args)
         print(f'Exception Ocurred, {message}\n')
-        removeDuplicate('links.txt')
     finally:
         print('All done :) \n')
     
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from allUrls import allUrls
 import requests
 from bs4 import BeautifulSoup
+import re
 
 def main():
     print('Starting execution\n')
@@ -15,9 +16,12 @@ def main():
     file = open('links.txt', 'a')
 
     for link in soup.find_all('a'):
-        links.append(link.get('href'))
-        
-    for link in links:
+        if type(link.get('href')) == str and re.match('^http', link.get('href')):
+            links.append(link.get('href'))
+    
+    newLinks = list(dict.fromkeys(links))
+   
+    for link in newLinks:
         if type(link) == type('a'):
             file.write(link)
             file.write('\n')
